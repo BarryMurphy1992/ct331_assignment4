f03ad7d10a7f0ded55281d212893f6fb9caf0469
BarryMurphy1992 <b.murphy50@nuigalway.ie>
2017-11-16 21:37:20 +0000

    Update assignment.pl

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,35 @@
 %Definitions for isElementInList(El, List)
+isElementInList(X, [X|T]).
+
+isElementInList(X, [H|T]):- 
+isElementInList(X, T).
+
 
 %Definitions for reverseList(List, ReversedList)
+reverseList([], ReversedList).
 
-%insertElementIntoListEnd(El, List, NewList)
+reverseList([H|T], ReversedList):-
+	reverseListMerge(T, [H], ReversedList).
+	
+reverseListMerge([],X,X).
+
+reverseListMerge([H|T],X,ReversedList):-
+		reverseListMerge(T, [H|X], ReversedList).
+
+%insertElementIntoListEnd(List, El, NewList)
+insertElementIntoListEnd([],El, [El]).
+
+insertElementIntoListEnd([H|T], El, [H|R]) :- 
+	insertElementIntoListEnd(T, El, R).
+
 
 %Definitions for mergeLists(List1, List2, Merged)
+
+mergeLists([],X,X).
+
+
+mergeLists([H|T], List2, [H|R]):-
+	mergeLists(T,List2,R).
+	
+	
+	
